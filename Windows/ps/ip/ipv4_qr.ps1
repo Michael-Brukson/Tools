01957570887ad3097e2d437ec49fe2e2c4947be6
Michael <michael.brukson@my.liu.edu>
2025-11-07 17:17:18 -0500

    Updated ipv4_qr to invoke curl.exe instead of curl, now properly printing the ASCII qr code. Additionally, on occurance of several IP addresses, it will select the last one. Modified verbose printing.

--- Windows/ps/ip/ipv4_qr.ps1
+++ Windows/ps/ip/ipv4_qr.ps1
@@ -7,14 +7,16 @@ param (
     [int]$PORT = 80
 )
 
-$IPV4 = &.\ipv4.ps1
+$IPV4 = &.\ipv4.ps1 | ForEach-Object { $_.Trim() } | Where-Object { $_ -ne "" } | Select-Object -Last 1 
+
 $PROT = if ($SEC) {"https"} else {"http"}
+[string]$URL = "https://qrenco.de/{0}://{1}:{2}" -f $PROT, $IPV4, $PORT
 
-if ($VerbosePreference) {
-    Write-Host "Secure?: " $SEC
-    Write-Host "Port: " $PORT
-    Write-Host "Host: " $IPV4
-    Write-Host "Curling: " $PROT"://"$IPV4":"$PORT
-}
 
-curl qrenco.de/$PROT"://"$IPV4":"$PORT
+Write-Verbose ("Secure?: {0}" -f $SEC)
+Write-Verbose ("Port: {0}" -f $PORT)
+Write-Verbose ("Host: {0}" -f $IPV4)
+Write-Verbose ("Curling: {0}" -f $URL)
+
+# & curl.exe "https://qrenco.de/{0}" -f $URL --silent --location --insecure\
+& curl.exe $URL
