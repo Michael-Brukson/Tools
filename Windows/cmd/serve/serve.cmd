0e0f51950e1eedaae8874e1044f2f2314ccee171
Mute <mbrukson@gmail.com>
2025-11-12 11:10:36 -0500

    Added serve.cmd, added serve.ps1. Documentation needed.

---
+++ Windows/cmd/serve/serve.cmd
@@ -0,0 +1,47 @@
+@echo off
+setlocal
+setlocal enabledelayedexpansion
+
+set "PORT=80"
+set "VERBOSE=0"
+for /f "usebackq delims=" %%a in (`call cd`) do set "DIR=%%a"
+
+
+:PARSE_ARGS
+if "%~1" == "" goto END_PARSE
+
+if /I "%~1" == "-verbose" (
+    set "VERBOSE=1"
+) else if /I "%~1" == "-v" (
+	set "VERBOSE=1"
+	
+) else if /I "%~1" == "-port" (
+    set "PORT=%~2"
+    shift
+) else if /I "%~1" == "-p" (
+	set "PORT=%~2"
+    shift
+
+) else if /I "%~1" == "-dir" (
+    set "DIR=%~2"
+    shift
+)else if /I "%~1" == "-d" (
+    set "DIR=%~2"
+    shift
+)
+
+shift
+goto PARSE_ARGS
+
+:END_PARSE
+
+if "%VERBOSE%"=="1" (
+	echo Port: %PORT%
+	echo Serving directory: !DIR!
+)
+
+call ipv4_qr -p %PORT%
+call python -m http.server %PORT% --bind 0.0.0.0 --directory !DIR!
+
+
+endlocal
--- Windows/ps/serve/serve.ps1
+++ Windows/ps/serve/serve.ps1
@@ -1,8 +1,15 @@
 [CmdletBinding()]
 param (
     [Alias('p')]
-    [int]$PORT = 80
+    [int]$PORT = 80,
+
+    [Alias('d')]
+    [string]$DIR = (pwd).Path
 )
 
-$IPV4 &.\ipv4.ps1
+Write-Verbose ("Directory: {0}" -f $DIR)
+
+# TODO: Known issue, currently ps1 scripts are not signed, and thus cannot be called even when added to environment paths.
+# & ipvr_qr -Argument1 "-v"
+& python -m http.server $PORT --bind 0.0.0.0 --directory $DIR
 
